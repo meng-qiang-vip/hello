bee955ca1cb1d6b691dd749b5bde6f07b53fefcc
mq <mengqiang@mengqiang>
2018-12-03 12:39:16 +0800

    增加新类

---
+++ src/SayTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mengqiang
+ * Date: 2018/12/3
+ * Time: 12:38 PM
+ */
+
+namespace Hello;
+
+
+class SayTest
+{
+
+    public function getTitle()
+    {
+        return "mengqiang";
+    }
+
+}
