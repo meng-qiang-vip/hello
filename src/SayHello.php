37dfa62009f38808e1d9433e7b033162b0f1fdb0
mq <mengqiang@mengqiang>
2018-12-03 11:23:17 +0800

    初始化项目

---
+++ src/SayHello.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mengqiang
+ * Date: 2018/12/3
+ * Time: 11:09 AM
+ */
+
+namespace Hello;
+
+class SayHello
+{
+        public static function say(){
+            return "hello world";
+        }
+
+}
---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mengqiang
+ * Date: 2018/12/3
+ * Time: 11:20 AM
+ */
+
+require_once  __DIR__.'/vendor/autoload.php';
+
+use Hello\SayHello;
+
+echo SayHello::say();
